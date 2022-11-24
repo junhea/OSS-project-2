a7f926e7340edae70f07b4ddf34be5490e027b5d
junhea <97426534+junhea@users.noreply.github.com>
2022-11-24 09:56:00 +0900

    function implementation

--- template.py
+++ template.py
@@ -1,25 +1,61 @@
-#PLEASE WRITE THE GITHUB URL BELOW!
-#
+# PLEASE WRITE THE GITHUB URL BELOW!
+# https://github.com/junhea/OSS-project-2/blob/main/template.py
 
 import sys
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 
 def load_dataset(dataset_path):
-	#To-Do: Implement this function
+    # To-Do: Implement this function
+    return pd.read_csv(dataset_path)
 
-def dataset_stat(dataset_df):	
-	#To-Do: Implement this function
+
+def dataset_stat(dataset_df):
+    # To-Do: Implement this function
+    cnt = dataset_df.groupby('target').size()
+    return ((dataset_df.columns != 'target').sum(), cnt[0], cnt[1])
+
 
 def split_dataset(dataset_df, testset_size):
-	#To-Do: Implement this function
+    # To-Do: Implement this function
+    X = dataset_df.drop(columns='target', axis=1)
+    y = dataset_df['target']
+    return train_test_split(X, y, test_size=testset_size)
+
 
 def decision_tree_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
+    # To-Do: Implement this function
+    dt_cls = DecisionTreeClassifier()
+    dt_cls.fit(x_train, y_train)
+    y_pred = dt_cls.predict(x_test)
+    return (accuracy_score(y_test, y_pred), precision_score(y_test, y_pred), recall_score(y_test, y_pred))
+
 
 def random_forest_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
+    # To-Do: Implement this function
+    rf_cls = RandomForestClassifier()
+    rf_cls.fit(x_train, y_train)
+    y_pred = rf_cls.predict(x_test)
+    return (accuracy_score(y_test, y_pred), precision_score(y_test, y_pred), recall_score(y_test, y_pred))
+
 
 def svm_train_test(x_train, x_test, y_train, y_test):
-	#To-Do: Implement this function
+    # To-Do: Implement this function
+    svm_pipe = make_pipeline(
+        StandardScaler(),
+        SVC()
+    )
+    svm_pipe.fit(x_train, y_train)
+    y_pred = svm_pipe.predict(x_test)
+    return (accuracy_score(y_test, y_pred), precision_score(y_test, y_pred), recall_score(y_test, y_pred))
+
 
 def print_performances(acc, prec, recall):
 	#Do not modify this function!
